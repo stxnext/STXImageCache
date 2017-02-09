1d8f26f7169e47cc8cd9da9a57bdf585abc64bf1
Norbert Sroczyński <norbert.sroczynski@gmail.com>
2017-02-09 15:29:22 +0100

    Fixed demo app

--- STXImageCacheDemo/ViewController.swift
+++ STXImageCacheDemo/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         
         var diskConfig = STXDiskCacheConfig()
         diskConfig.enabled = true
-        diskConfig.diskExpirationTime = 7 // in days, 0 = never (dependent on iOS)
+        diskConfig.cacheExpirationTime = 7 // in days, 0 = never (dependent on iOS)
         STXCacheManager.shared.diskCacheConfig = diskConfig
         
         var memoryConfig = STXMemoryCacheConfig()
@@ -26,20 +26,9 @@ class ViewController: UIViewController {
         memoryConfig.maximumMemoryCacheSize = 50 // in megabytes, 0 = unlimited
         STXCacheManager.shared.memoryCacheConfig = memoryConfig
         
-        for i in 0...1000 {
-            let url = URL(string: "https://unsplash.it/200/300/?random")!
-            STXCacheManager.shared.image(atURL: url, forceRefresh: true, completion: { data, error in
-                if let data = data {
-                    print(i)
-                }
-            })
-        }
-        
         let url = URL(string: "https://s3-us-west-1.amazonaws.com/powr/defaults/image-slider2.jpg")!
         firstImageView.stx.image(atURL: url)
         button.stx.image(atURL: url)
-        
-        STXCacheManager.shared.clearCache()
     }
 }
 
